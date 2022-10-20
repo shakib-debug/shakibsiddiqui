019e5adfb7d607c13aa590bf57c1755d303538fa
Shakib <shakibsiddiqui63@gmail.com>
2022-10-20 13:39:44 -0700

    first commit

---
+++ employee_management_frontend/src/app/employee-details/employee-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css']
+})
+export class EmployeeDetailsComponent implements OnInit {
+
+  id!: number;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+    ngOnInit(): void {
+      this.id = this.route.snapshot.params['id'];
+      this.employeeService.getEmployeeById(this.id).subscribe(data => {
+        this.employee = data;
+      });
+    }
+
+
+}
